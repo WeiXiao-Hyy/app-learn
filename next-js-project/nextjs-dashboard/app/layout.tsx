b832935fecdca183c78664e75e519af36554cd6e
hujiale <102018984+WeiXiao-Hyy@users.noreply.github.com>
2024-06-08 14:37:10 +0800

    next-js-dashboard

--- next-js-project/nextjs-dashboard/app/layout.tsx
+++ next-js-project/nextjs-dashboard/app/layout.tsx
@@ -1,14 +1,15 @@
 import '@/app/ui/global.css'
-import { inter } from './ui/fonts';
+import {inter} from './ui/fonts';
+import React from "react";
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <body className={`${inter.className} antialiasing`}>{children}</body>
     </html>
   );
 }
--- next-js-project/nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ next-js-project/nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import {PowerIcon} from '@heroicons/react/24/outline';
 
 export default function SideNav() {
   return (
@@ -11,15 +11,16 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+          <AcmeLogo/>
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
+        <NavLinks/>
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
+          <button
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6"/>
             <div className="hidden md:block">Sign Out</div>
           </button>
         </form>
