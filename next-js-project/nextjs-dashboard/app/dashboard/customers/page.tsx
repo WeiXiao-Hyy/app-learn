a0ff115b637223bf3adf3b9362bd5f4d13eea3dd
hujiale <102018984+WeiXiao-Hyy@users.noreply.github.com>
2024-06-08 14:43:14 +0800

    mv project

--- next-js-project/nextjs-dashboard/app/dashboard/customers/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-  return (
-    <div>Customers Page</div>
-  )
-}
--- next-js-project/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-  return (
-    <div>Invoices Page</div>
-  )
-}
